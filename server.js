3a322fa8414e78b8edc3246d12567125907aba9b
Patrick Poopathi <patrickgp1992@hotmail.com>
2022-08-02 19:31:18 -0400

    completed incorporating a dataset

--- server.js
+++ server.js
@@ -1,22 +1,20 @@
-const fs = require("fs");
-const path = require("path");
 const express = require("express");
-const { animals } = require("./data/animals");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
 const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
-
-const app = express();
 
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parese incoming JSON data
 app.use(express.json());
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes);
 // express.static() instructs the server to make all the files inside "" static resources
 app.use(express.static("public"));
 
-const PORT = process.env.PORT || 3001;
+// use apiRoutes
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
